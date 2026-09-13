40c882756649607050a6673c1423e54ff6e31a98
nnnc8 <266626716+nnnc8@users.noreply.github.com>
2026-09-13 11:34:06 +0800

    refactor: remove unreachable V1 frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-"use client";
-
-import dynamic from "next/dynamic";
 import { V2LiffHome } from "@/components/ledger/v2-liff-home";
 
-const LegacyHome = dynamic(() => import("@/components/legacy-home"), { ssr: false });
-
 export default function Home() {
-  return process.env.NEXT_PUBLIC_V2_LEDGER_UI === "1" ? <V2LiffHome /> : <LegacyHome />;
+  return <V2LiffHome />;
 }
